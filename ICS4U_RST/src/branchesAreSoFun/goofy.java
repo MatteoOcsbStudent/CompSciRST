b4193251f47e3aa45bafadbe970e8d6e78794a2c
s445958 <s445958@shha216m50.ocsb.local>
2022-12-09 14:10:29 -0500

    whoops

--- ICS4U_RST/src/branchesAreSoFun/goofy.java
+++ ICS4U_RST/src/branchesAreSoFun/goofy.java
@@ -7,6 +7,6 @@ public class goofy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Console.print("gotta love branches");
+		Console.print("gooooooofy");
 	}
-
 }
