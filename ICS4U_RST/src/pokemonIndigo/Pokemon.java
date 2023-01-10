57d4a21ef7592b708b7467683b7b2f4e427e73e8
s445958 <s445958@shha216m50.ocsb.local>
2023-01-10 14:12:44 -0500

    Pokemob object class logic and file reading

--- ICS4U_RST/src/pokemonIndigo/Pokemon.java
+++ ICS4U_RST/src/pokemonIndigo/Pokemon.java
@@ -16,7 +16,7 @@ public class Pokemon {
 	int baseAtk;
 	int baseDef;
 	int baseSpd;
-	int level;
+	int level = 0;
 	int nextLevelUp;
 	int exp;
 	int currentHP;
@@ -32,18 +32,23 @@ public class Pokemon {
 	Image spriteBack;
 	
 	String [] types = new String[2];
+	String [] evolutions;
+	int [] evoLevels;
 	
-	ArrayList <Integer> evoLevels = new ArrayList <Integer>();
 	ArrayList <Move> movePool = new ArrayList <Move>();
 	ArrayList <Integer> movePoolLevels = new ArrayList <Integer>();
 	ArrayList <Move> currentMoves = new ArrayList <Move>();
 
-	public Pokemon(String species, int level) {
+	public Pokemon(String species, int foundLevel) {
+		
+		//Setting name
 		name = species;
 		
-		Image spriteFront = new Image(getClass().getResource("/images/PokemonSprites/" + name + "Front.png").toString());
-		Image spriteBack = new Image(getClass().getResource("/images/PokemonSprites/" + name + "Back.png").toString());
+		//Setting sprite based on pokemon name
+		spriteFront = new Image(getClass().getResource("/images/PokemonSprites/" + name + "Front.png").toString());
+		spriteBack = new Image(getClass().getResource("/images/PokemonSprites/" + name + "Back.png").toString());
 		
+		//Pulling information from file based on pokemon's name
 		File pokemonInfo = new File("data/pokemonFiles/" + name + ".txt");
 		
 		FileReader pokeFileReader;
@@ -52,19 +57,48 @@ public class Pokemon {
 			pokeFileReader = new FileReader(pokemonInfo);
 			BufferedReader pokeStream = new BufferedReader(pokeFileReader);
 
+			//Reading pokemon's types, returns null for second type if there is none
 			types[0] = pokeStream.readLine();
 			types[1] = pokeStream.readLine();
 			
+			//Reads base stats
 			baseHP = Integer.parseInt(pokeStream.readLine());
 			baseAtk = Integer.parseInt(pokeStream.readLine());
 			baseDef = Integer.parseInt(pokeStream.readLine());
 			baseSpd = Integer.parseInt(pokeStream.readLine());
 			
+			//amount of moves to be read
 			int amountOfMoves = Integer.parseInt(pokeStream.readLine());
 			
+			//Reading move names and levels at which they're learned
 			for (int i = 0; i < amountOfMoves; i++) {
 				movePool.add(new Move(pokeStream.readLine()));
 				movePoolLevels.add(Integer.parseInt(pokeStream.readLine()));
+			}
+			
+			//Amount of evolutions available
+			int amountOfEvos = Integer.parseInt(pokeStream.readLine());
+			
+			//Declaring evolution data fields 
+			evolutions = new String [amountOfEvos];
+			evoLevels = new int [amountOfEvos];
+			
+			//Reading evolution names and levels at which evolution happens
+			for (int i = 0; i < amountOfEvos; i++) {
+				evolutions[i] = pokeStream.readLine();
+				
+				//Skipping base stats
+				for (int skip = 0; skip < 4; skip++) {
+					pokeStream.readLine();
+				}
+				
+				evoLevels[i] = Integer.parseInt(pokeStream.readLine());
+				
+				//Skipping evolution types
+				for (int skip = 0; skip < 2; skip++) {
+					pokeStream.readLine();
+				}
+				
 			}
 			
 			
@@ -77,28 +111,30 @@ public class Pokemon {
 		}
 		
 		
-		for (int i = 1; i < level; i++) {
+		for (int i = 0; i < foundLevel; i++) {
 			levelUp();
 		}
 	}
 	
 	public void levelUp () {
 		level++;
-		
 		//HP Stat
-		int hpIncrease = (int) Math.ceil(baseHP/50);
-		totalHP += hpIncrease;
-		currentHP += hpIncrease;
+		//int hpIncrease = (int) Math.ceil(baseHP/25.0);
+		//totalHP += hpIncrease;
+		//currentHP += hpIncrease;
+		
+		totalHP = ((2*baseHP*level)/100) + level + 10;
 		
 		//Atk stat
-		int atkIncrease = (int) Math.ceil(baseAtk/50);
+		int atkIncrease = (int) Math.ceil(baseAtk/25.0);
 		attack += atkIncrease;
 		
 		//Def stat
-		int defIncrease = (int) Math.ceil(baseDef/50);
+		int defIncrease = (int) Math.ceil(baseDef/25.0);
 		defense += defIncrease;
 		
-		int spdIncrease = (int) Math.ceil(baseSpd/50);
+		//Spd stat
+		int spdIncrease = (int) Math.ceil(baseSpd/25.0);
 		speed += spdIncrease;
 	}
 	
@@ -170,9 +206,9 @@ public class Pokemon {
 		
 		int nextEvo = -1;
 		
-		for (int i = 0; i < 3; i++) {
-			if (evoLevels.get(i) > level) {
-			nextEvo = evoLevels.get(i);
+		for (int i = 0; i < 1; i++) {
+			if (evoLevels[i] > level) {
+			nextEvo = evoLevels[i];
 			}
 		}
 		return nextEvo;
